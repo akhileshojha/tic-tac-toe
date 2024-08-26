7a6455de5a59916a715bd0aba7b3820557ff229f
Akhilesh Ojha <33337618+akhileshojha@users.noreply.github.com>
2024-08-26 17:58:37 +0530

    tic tac toe game development

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Board from './Square';
+import Game from './Game';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Board />
+     <Game />
     </div>
   );
 }
